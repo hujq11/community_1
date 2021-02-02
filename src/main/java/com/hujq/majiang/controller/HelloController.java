c33c654dc34f6381138a5ea850e44e69369ab71f
hujq11 <2367106751@qq.com>
2021-02-02 18:37:37 +0800

    init repo

---
+++ src/main/java/com/hujq/majiang/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.hujq.majiang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+
+}
---
+++ src/test/java/com/hujq/majiang/MajiangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hujq.majiang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajiangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
